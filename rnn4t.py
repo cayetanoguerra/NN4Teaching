bf1beaec330affe3ac977142cd8e1696cfb98ee1
Cayetano <cayetanoguerra@gmail.com>
2016-03-26 11:24:08 +0000

    Change of rnn
    Creation of rnn4t from nn4t

--- rnn4t.py
+++ rnn4t.py
@@ -1,9 +1,23 @@
-"""
-Simple demo of an oscillator net
-"""
+#
+#   Neural Nets for Teaching
+#
+#   Cayetano Guerra Artal (c) 2016
+#
+#
+
 
 import numpy as np
-import matplotlib.pyplot as plt
+
+
+# Translate a list of labels into an array of 0's and one 1.
+# i.e.: 4 -> [0,0,0,0,1,0,0,0,0,0]
+def one_hot(x, n):
+    if type(x) == list:
+        x = np.array(x)
+    x = x.flatten()
+    o_h = np.zeros((len(x), n))
+    o_h[np.arange(len(x)), x] = 1
+    return o_h
 
 
 #
@@ -29,7 +43,7 @@ def relu(x):
 
 def relu_derivate(o):
     if o >= 0.:
-        return 1
+        return 1.
     else:
         return 0.
 
@@ -42,21 +56,21 @@ def identity(x):
 
 
 def identity_derivate():
-    return 1
+    return 1.
 
 
 #
 # TanH
 #
 def tanh(x):
-    return 2/(1 + np.exp(-2*x)) - 1
+    return 2./(1. + np.exp(-2. * x)) - 1.
 
 
 def tanh_derivate(o):
-    return 1 - o**2
+    return 1. - o**2.
 
 
-activation_function = {
+activ_f = {
     "Sigmoid": (sigmoid, sigmoid_derivate),
     "ReLU": (relu, relu_derivate),
     "Identity": (identity, identity_derivate),
@@ -68,10 +82,10 @@ class Layer:
     """ Layer
     """
 
-    def __init__(self, rows, columns, activ_f=activation_function["Sigmoid"]):
+    def __init__(self, rows, columns, activ_f=activ_f["Sigmoid"]):
         self.rows = rows
         self.columns = columns + 1  # One column for bias
-        self.W = self.create_W(self.rows, self.columns)
+        self.W = Layer.create_W(self.rows, self.columns)
         self.W_delta = []
         self.b_delta = []
         self.activ_f = activ_f[0]
@@ -79,7 +93,8 @@ class Layer:
         self.error = []
         self.o = None
 
-    def create_W(self, rows, columns):
+    @staticmethod
+    def create_W(rows, columns):
         return np.random.rand(rows, columns) * 0.1
 
     def output(self, x):
@@ -92,69 +107,38 @@ class Layer:
         return "Layer_____________\n     #inputs:" + repr(self.columns-1) + "\n     #neurons: " + repr(self.rows)
 
 
-class RNNLayer:
-    """ Layer
+class Net:
+    """ Net
     """
 
-    def __init__(self, number_of_neurons, external_inputs, activ_f=activation_function["Sigmoid"]):
-        self.neurons = number_of_neurons
-        self.external_inputs = external_inputs
-        self.W = self.create_W(self.neurons, self.neurons + self.external_inputs)
-        self.activ_f = activ_f[0]
-        self.o_activ_f_derivate = activ_f[1]
-        self.error = []
-        self.o = None
-
-    def create_W(self, rows, columns):
-        return np.random.rand(rows, columns) * 10. - 5.5
+    def __init__(self, layers):
+        self.layers = []
+        l_prev = layers[0]
+        for l in layers[1:]:
+            self.layers.append(Layer(l, l_prev))
+            l_prev = l
 
     def output(self, x):
-        self.o = self.activ_f(np.dot(self.W, x)) #x first neuron outputs, after that external inputs.
-        self.o_derivate = self.o_activ_f_derivate(self.o)
-        return self.o
+        o_aux = x
+        for layer in self.layers:
+            o_aux = layer.output(o_aux)
+        return o_aux
 
-    def __str__(self):
-        return "Layer_____________\n     #inputs:" + repr(self.columns) + "\n     #neurons: " + repr(self.rows)
+    def train(self, input_data, labels, lr=0.1):
+        for j in range(len(input_data)):
+            x = input_data[j]
+            _y = labels[j]
+            y = self.output(x)
+            error = y - _y
+            for index_layer in range(len(self.layers))[:0:-1]:
+                error = error * self.layers[index_layer].o_derivate
+                self.layers[index_layer].W_delta = np.dot(error.reshape((len(error), 1)),
+                                                          (np.append(self.layers[index_layer-1].o, 1.0))
+                                                          .reshape(1, len(self.layers[index_layer-1].o)+1))
+                error = np.dot(error, self.layers[index_layer].W[:, 0:-1])
 
+            error = error * self.layers[0].o_derivate
+            self.layers[0].W_delta = np.dot(error.reshape((len(error), 1)), np.append(x, 1.0).reshape((1, len(x)+1)))
 
-neurons = 5
-inputs = 2
-
-hidden_layer = RNNLayer(number_of_neurons=neurons, external_inputs=inputs)
-output_layer = Layer(1, neurons)
-
-original_inputs = np.zeros(neurons)
-inputs = np.asarray([1., 1.])
-
-complete_input = np.append(original_inputs, inputs)
-
-y = np.asarray([])
-
-for i in xrange(50):
-
-    if i < 50:
-        inputs = np.zeros(2)
-        #inputs = np.random.rand(2)
-    elif i < 100:
-        inputs = np.zeros(2)
-    else:
-        inputs = np.ones(2)
-
-    #print "Complete inputs: ", complete_input
-
-    h_o = hidden_layer.output(complete_input)
-    complete_input = np.append(h_o, inputs)
-    print "Hidden layer: ", h_o
-    o = output_layer.output(h_o)
-    y = np.append(y, h_o)
-
-y = y.reshape((50, 5))
-
-plt.plot(y[:, 0], 'b')
-plt.plot(y[:, 1], 'r')
-plt.plot(y[:, 2], 'g')
-plt.plot(y[:, 3], 'y')
-plt.plot(y[:, 4], 'k')
-
-plt.show()
-
+            for index_layer in range(len(self.layers)):
+                self.layers[index_layer].W -= self.layers[index_layer].W_delta * lr
