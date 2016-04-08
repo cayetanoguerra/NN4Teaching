b6ab782482aee5d942c09ec2260987642ff9c0c5
Cayetano <cayetanoguerra@gmail.com>
2016-04-08 11:59:04 +0100

    legible

--- perceptron.py
+++ perceptron.py
@@ -18,7 +18,7 @@ def sigmoid_derivate(o):
 
 def train(x_data, y_data):
 
-    w1, w2, b = np.random.rand(3)
+    w0, w1, w2 = np.random.rand(3)
     lr = 0.1
 
     print "Training..."
@@ -27,29 +27,27 @@ def train(x_data, y_data):
         
         w1_d = []
         w2_d = []
-        b_d = []
+        w0_d = []
         
         for data, label in zip(x_data, y_data):
 
-            o = sigmoid(w1*data[0] + w2*data[1] + b)
+            o = sigmoid(w0*1.0 + w1*data[0] + w2*data[1])
             error = 2.*(o - label) * sigmoid_derivate(o)
 
+            w0_d.append(error * 1.0)
             w1_d.append(error * data[0])
             w2_d.append(error * data[1])
-            b_d.append(error)
-
+            
+        w0 = w0 - np.sum(w0_d) * lr
         w1 = w1 - np.sum(w1_d) * lr
         w2 = w2 - np.sum(w2_d) * lr
-        b = b - np.sum(b_d) * lr
-
+        
+        
     for data, label in zip(x_data, y_data):
         print data, "->", label
-        o = sigmoid(w1*data[0] + w2*data[1] + b)
+        o = sigmoid(w0*1.0 + w1*data[0] + w2*data[1])
         print o
         print "-----------------------"
 
 
 train(x_data, y_data)
-
-
-
